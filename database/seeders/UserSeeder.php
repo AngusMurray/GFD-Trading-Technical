6675043802fd1487cf3184cb40c48e087b54cfe7
Angus Murray <angusmurray@outlook.com>
2024-04-29 15:08:21 +0100

    Setup DB connection and tested seeding data

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class user extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
