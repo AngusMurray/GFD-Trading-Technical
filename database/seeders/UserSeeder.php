006d58d26384ef637bad64375f89a3d7e83d7587
Angus Murray <angusmurray@outlook.com>
2024-04-29 18:00:07 +0100

    Update UserSeeder.php
    - Adding management user to user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,25 @@
 
 namespace Database\Seeders;
 
+use App\Models\Department;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
+    protected static ?string $password;
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
         User::factory(10)->create();
+
+        collect([
+            ['name' => 'Management User', 'email' => 'management.user@company.co.uk', 'email_verified_at' => now(), 'password' => static::$password ??= Hash::make('qwerty123'), 'department_id' => Department::inRandomOrder()->first()->id, 'isManagement' => true]
+        ])->each(function ($seed){
+            User::create($seed);
+        });
     }
 }
